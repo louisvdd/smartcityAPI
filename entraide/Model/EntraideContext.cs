d2179935a81714dd86eee4dfb006eb4f64a93729
louisvdd <etu28841@student.henallux.be>
2016-11-19 16:03:11 +0100

    model

--- entraide/Model/EntraideContext.cs
+++ entraide/Model/EntraideContext.cs
@@ -9,12 +9,12 @@ namespace Model
 {
     public class EntraideContext : DbContext
     {
-        public DbSet<Service>Services { get; set; }
-        public DbSet<Comment>Comments { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Comment> Comments { get; set; }
         public DbSet<DoService> DoServices { get; set; }
         public DbSet<CategoryService> TypeServices { get; set; }
         public DbSet<User> Users { get; set; }
-        public EntraideContext(): base(@"Data Source = g-aideserv.database.windows.net; Initial Catalog = g-aideSQLDAta; Integrated Security = False; User ID = sponcho; Password=Beguinvdd123;Connect Timeout = 60; Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
+        public EntraideContext() : base(@"Data Source = g-aideserv.database.windows.net; Initial Catalog = g-aideSQLDAta; Integrated Security = False; User ID = sponcho; Password=Beguinvdd123;Connect Timeout = 60; Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
         {
 
         }
--- entraide/Model/Properties/AssemblyInfo.cs
+++ entraide/Model/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Model")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ba511a58-3487-487f-8e75-53bd903d060f")]
+[assembly: Guid("f7782634-81e9-4184-94de-6ea3d88c4ee2")]
 
 // Version information for an assembly consists of the following four values:
 //
